3a176b8651734a5a5ff6c19382e48e57655d78c3
Gergely Németh <ngerle@gmail.com>
2017-04-30 23:33:24 +0200

    Add list_page() to views

--- app/views.py
+++ app/views.py
@@ -1,7 +1,16 @@
+from flask import render_template, Markup
 from app import app
 
 
 @app.route('/')
-@app.route('/index')
-def hello_world():
-    return "Hello, World!"
+@app.route('/list')
+def list_page():
+    with open("app/data.csv") as f:
+        data = []
+        for line in f.read().split("\n"):
+            data_line = []
+            for field in line.split(';'):
+                field = Markup(field.replace("\\n", "<br />"))
+                data_line.append(field)
+            data.append(data_line)
+    return render_template('list.html', data=data, enumerate=enumerate)
